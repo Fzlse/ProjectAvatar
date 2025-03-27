6f29a2b3a7b235a596d37b0d448853925f5d5b7d
Fzlse <andratml3@gmail.com>
2025-03-27 14:42:18 +0700

    fix coliider
    fixing some offset collider

--- Assets/Script/New/Player/PlayerCollision.cs
+++ Assets/Script/New/Player/PlayerCollision.cs
@@ -10,26 +10,40 @@ public class PlayerCollisionHandler : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         playerController = GetComponent<PlayerController>();
+
+        Debug.Log("PlayerCollisionHandler initialized.");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+
         if (collision.gameObject.CompareTag("Obstacle"))
         {
+            Debug.Log("Player collided with an obstacle!");
+
             if (playerController != null)
             {
                 playerController.enabled = false;
+                Debug.Log("PlayerController disabled.");
             }
+
             rb.velocity = new Vector2(0, -10f);
             isFalling = true;
+            Debug.Log("Player set to falling state with velocity (-10f).");
         }
     }
 
     private void Update()
     {
-        if (isFalling && transform.position.y < -10f)
+        if (isFalling)
         {
-            Debug.Log("Player jatuh terlalu jauh!");
+            Debug.Log("Player is currently falling...");
+
+            if (transform.position.y < -10f)
+            {
+                Debug.LogWarning("Player jatuh terlalu jauh!");
+            }
         }
     }
 }
